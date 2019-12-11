6351e3df60d56045c7fd9e647218f96c35b62e56
Maxim Moskalenko <maksm.84@gmail.com>
2019-12-12 01:00:09 +0300

    Module

---
+++ Module.js
@@ -0,0 +1,30 @@
+const footballTransfers = (() => {
+  const players = [];
+
+  return {
+    addPlayer(player) {
+      players.push(player);
+    },
+    showAll() {
+      players.forEach(p => {
+        console.log(p.name, p.surname, p.price);
+      });
+    }
+  };
+})();
+
+const CR7 = {
+  name: 'Cristiano',
+  surname: 'Ronaldo',
+  price: '$110.000.000'
+};
+
+const CaretBale = {
+  name: 'Caret',
+  surname: 'Bale',
+  price: '$70.000.000'
+};
+
+footballTransfers.addPlayer(CR7);
+footballTransfers.addPlayer(CaretBale);
+footballTransfers.showAll();
