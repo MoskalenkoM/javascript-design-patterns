346f99ffa319b3f8d18a72fd6a789f929d6ac5aa
Maxim Moskalenko <maksm.84@gmail.com>
2019-12-12 00:55:35 +0300

    Singleton

---
+++ Singleton.js
@@ -0,0 +1,34 @@
+// Singleton is a creational pattern.
+// Singleton ensures that the class has only one instance.
+
+class Singleton {
+  constructor() {
+    if (typeof Singleton.instance === 'object') {
+      return Singleton.instance;
+    }
+
+    this.count = 0;
+    Singleton.instance = this;
+    return this;
+  }
+
+  getCount() {
+    return this.count;
+  }
+
+  increaseCount() {
+    return this.count++;
+  }
+}
+
+const singleton1 = new Singleton();
+const singleton2 = new Singleton();
+
+singleton1.increaseCount();
+singleton1.increaseCount();
+singleton2.increaseCount();
+singleton2.increaseCount();
+singleton2.increaseCount();
+
+console.log(singleton1.getCount());
+console.log(singleton2.getCount());
